4036323285f9d58626888c3768670da72673dabb
David <david.carrasco.cunado@gmail.com>
2021-12-18 19:56:20 +0100

    feat(naming): keys and values front-back

--- src/portfoliovisualizerapi/common/naming.py
+++ src/portfoliovisualizerapi/common/naming.py
@@ -1,40 +1,141 @@
 """
-Correspondence naming between www.portfoliovisualizer.com input and backend parameters
+Correspondence naming between www.portfoliovisualizer.com front and backend parameters
 """
 
 NAMING_KEYS = {
-    'Time Period': 'timePeriod',
-    'Start Year': 'startYear',
-    'First Month': 'firstMonth',
-    'End Year': 'endYear',
-    'Last Month': 'lastMonth',
-    'Calendar Aligned': 'calendarAligned',
-    'Include YTD': 'includeYTD',
-    'Initial Amount': 'initialAmount',
-    'Cashflows': 'annualOperation',  # 0,1,2,3
+    'Time Period': 'timePeriod',                    # Specify time period in years or in months
+    'Start Year': 'startYear',                      # Start year for portfolio backtest
+    'First Month': 'firstMonth',                    # Start month for portfolio backtest
+    'End Year': 'endYear',                          # End year for portfolio backtest
+    'Last Month': 'lastMonth',                      # End month for portfolio backtest
+    'Calendar Aligned': 'calendarAligned',          # Align quarterly and annual cashflows and rebalancing with calendar periods or count from the portfolio start month
+    'Include YTD': 'includeYTD',                    # Include the month-to-date period of the current month for YTD results
+    'Initial Amount': 'initialAmount',              # Initial portfolio balance
+    'Cashflows': 'annualOperation',                 # Specify whether any withdrawals or contributions are done
     'Contribution Amount': 'annualAdjustment',
     'Withdrawal Amount': 'annualAdjustment',
     'Withdrawal Percentage': 'annualPercentage',
-    'Inflation Adjusted': 'inflationAdjusted',
-    'Contribution Frequency': 'frequency', # 2 == monthly, 3 == quaterly, 4 == annualy
+    'Inflation Adjusted': 'inflationAdjusted',      # Adjust contribution or withdrawal amount for inflation
+    'Contribution Frequency': 'frequency',          # 2 == monthly, 3 == quaterly, 4 == annualy
     'Withdrawal Frequency': 'frequency',
-    'Rebalancing': 'rebalanceType',
-    'Absolute Deviation': 'absoluteDeviation',
-    'Relative Deviation': 'relativeDeviation',
-    'Leverage Type': 'leverageType',
-    'Levarage Ratio': 'leverageRatio',
-    'Debt Amount': 'debtAmount',
-    'Debt Interest': 'debtInterest',
-    'Maintenance Margin': 'maintenanceMargin',
-    'Leveraged Benchmark': 'leveragedBenchmark',
-    'Reinvest Dividends': 'reinvestDividends',
-    'Display Income': 'showYield',
-    'Factor Regression': 'showFactors',
-    'Equity Factor Model': 'factorModel',
-    'Benchmark': 'benchmark',
-    'Benchmark Ticker': 'benchmarkSymbol'
+    'Rebalancing': 'rebalanceType',                 # Specify if and how rebalancing is done
+    'Absolute Deviation': 'absoluteDeviation',      # Absolute percentage rebalancing corridor
+    'Relative Deviation': 'relativeDeviation',      # Relative percentage rebalancing corridor. Set to zero to disable relative allocation checks.
+    'Leverage Type': 'leverageType',                # Specify type of leverage used if any
+    'Levarage Ratio': 'leverageRatio',              # Leverage ratio (debt/equity)
+    'Debt Amount': 'debtAmount',                    # Amount of debt used for leverage
+    'Debt Interest': 'debtInterest',                # Annual interest rate on borrowed funds
+    'Maintenance Margin': 'maintenanceMargin',      # Maintenance margin requirement to prevent margin call
+    'Leveraged Benchmark': 'leveragedBenchmark',    # Specify whether leverage is also applied to the selected benchmark
+    'Reinvest Dividends': 'reinvestDividends',      # Reinvest dividends and distributions
+    'Display Income': 'showYield',                  # Display portfolio income based on dividends and distributions
+    'Factor Regression': 'showFactors',             # Display risk factor regression
+    'Equity Factor Model': 'factorModel',           # Specify the equity factor model
+    'Benchmark': 'benchmark',                       # Select benchmark index or an imported benchmark series
+    'Benchmark Ticker': 'benchmarkSymbol'           # Ticker symbol for the benchmark asset
 }
 
+
+# NOTE: float or int values are not incluided in the values since are passed directly to the backend
 NAMING_VALUES = {
-    
+    'Time Period': {
+        'Month-to-Month': 2,
+        'Year-to-Year': 4
+    },
+    'First Month': {
+        'Jan': 0,
+        'Feb': 1,
+        'Mar': 2,
+        'Apr': 3,
+        'May': 4,
+        'Jun': 5,
+        'Jul': 6,
+        'Aug': 7,
+        'Sep': 8,
+        'Oct': 9,
+        'Nov': 10,
+        'Dec': 11
+    },
+    'Last Month': {
+        'Jan': 0,
+        'Feb': 1,
+        'Mar': 2,
+        'Apr': 3,
+        'May': 4,
+        'Jun': 5,
+        'Jul': 6,
+        'Aug': 7,
+        'Sep': 8,
+        'Oct': 9,
+        'Nov': 10,
+        'Dec': 11
+    },
+    'Calendar Aligned': {
+        'Yes': 'true',
+        'No': 'false'
+    },
+    'include YTD': {
+        'Yes': 'true',
+        'No': 'false'
+    },
+    'Cashflows': {
+        'None': 0,
+        'Contribute fixed amount': 1,
+        'Withdraw fixed amount': 2,
+        'Withdraw fixed percentage': 3
+    },
+    'Inflation Adjusted': {
+        'Yes': 'true',
+        'No': 'false'
+    },
+    'Contribution Frequency': {
+        'Monthly': 2,
+        'Quaterly': 3,
+        'Annualy': 4
+    },
+    'Withdrawal Frequency': {
+        'Monthly': 2,
+        'Quaterly': 3,
+        'Annualy': 4
+    },
+    'Rebalancing': {
+        'No rebalancing': 0,
+        'Rebalance annually': 1,
+        'Rebalance semi-annually': 2,
+        'Rebalance quarterly': 3,
+        'Rebalance monthly': 4,
+        'Rebalance bands': 5 
+    },
+    'Leverage Type': {
+        'None': 0,
+        'Fixed Debt Amount': 2,
+        'Fixed Leverage Ratio': 1
+    },
+    'Leveraged Benchmark': {
+        'Yes': 'true',
+        'No': 'false'
+    },
+    'Reinvest Dividends': {
+        'Yes': 'true',
+        'No': 'false'
+    },
+    'Display Income': {
+        'Yes': 'true',
+        'No': 'false'
+    },
+    'Factor Regression': {
+        'Yes': 'true',
+        'No': 'false'
+    },
+    'Equity Factor Model': {
+        'Three-Factor Model': 0,
+        'Four-Factor Model': 1,
+        'Five-Factor Model': 2
+    },
+    'Benchmark': {
+        'None': '',
+        'Specify Ticker...': -1,
+        'Vanguard 500 Index Investor': 'VFINX',
+        'Vanguard Balanced Index Inv': 'VBINX',
+    }
 }
